5234935a5b38ef41e14e5faafd6a2fdc601330a0
aniket <dubeyaniket7839@gmail.com>
2024-02-04 19:38:32 +0530

    stack and list

--- doubly_ended_queue.cpp
+++ doubly_ended_queue.cpp
@@ -4,13 +4,18 @@
 using namespace std;
 int main (){
     deque<int> d;
-    d.push_back(1);// ye back se element insert kiya hai 
-    d.push_front(2);// ye front se element insert kiya hai 
+    d.push_back(1);  // ye back se element insert kiya hai 
+    d.push_front(2);  // ye front se element insert kiya hai 
     for (int i :d){
         cout<<i<<" ";
     }cout<<endl;
     d.pop_front();//shuru ka element pop kar diya hai 
     for (int i :d){
         cout<<i<<" ";
+    }cout<<endl;
+    d.erase(d.end());  //isme ye hota hai ki element delete karne ke liye ek range deni padti hai ya toh ek karna hai ya ek range me 
+    for (int i :d){
+        cout<<i<<" ";
     }
+
 }
---
+++ list.cpp
@@ -0,0 +1,13 @@
+//isme empty aur at use nahi hota hai jaise array aur wo deque me hota hai 
+// traverse karke waha tak jaana padega element nikalne ke liye 
+#include <iostream>
+#include <list>
+using namespace std;
+int main (){
+    list <int> l; // wahi sab same operation hote hai 
+    l.push_back(1);
+    l.push_front(2);
+    for (int i :l){
+        cout<<i<<" ";
+    }
+}
